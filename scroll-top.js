a71700174cfa1edd4d619594bec03f627d1b0d2a
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 23:54:30 +0300

    gallery-start

---
+++ scroll-top.js
@@ -0,0 +1,16 @@
+/* scroll to top button */
+let mybutton = document.getElementById("scroll-to-top-button");
+// When the user scrolls down 500px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
